b724e3a2b951d406921e48bbe71229cafaa4e5c4
Akshit Vig <akshitvig48@gmail.com>
2025-01-21 23:52:40 +0530

    migration done

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -7,32 +7,49 @@ import {
   CreateSignInSchema,
   CreateRoomSchema,
 } from "@repo/common/types";
+import { prismaClient } from "@repo/db/client";
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/signup", (req, res) => {
-  const data = CreateSignupSchema.safeParse(req.body);
-  if (!data.success) {
+app.post("/signup", async (req, res) => {
+  const ParsedData = CreateSignupSchema.safeParse(req.body);
+
+  if (!ParsedData.success) {
+    res.json({
+      warning: "failed ",
+    });
     return;
   }
-  res.json({
-    message: "fine",
-  });
+
+  try {
+    const user = await prismaClient.user.create({
+      data: {
+        email: ParsedData.data?.email,
+        password: ParsedData.data?.password,
+        name: ParsedData.data?.name,
+      },
+    });
+
+    res.json({
+      message: "user created successfully",
+      userId: user.id,
+    });
+  } catch (e) {
+    res.status(411).json({
+      message: "error while creating user or email already exists",
+    });
+  }
 });
 
 app.post("/signin", (req, res) => {
-  const signinData = CreateSignInSchema.safeParse(req.body());
-  if (!signinData) {
+  const parsedData = CreateSignInSchema.safeParse(req.body());
+  if (!parsedData.success) {
     return;
   }
-  const userId = 2;
-  const token = jwt.sign({ userId }, JWT_SECRET);
 
-  res.json({
-    token,
-  });
+  con;
 });
 
 app.post("/createroom", auth, (req, res) => {
--- packages/common/src/types.ts
+++ packages/common/src/types.ts
@@ -1,15 +1,14 @@
 import { z } from "zod";
 
 export const CreateSignupSchema = z.object({
-  username: z.string().min(3).max(20),
+  email: z.string().email(),
   password: z.string().min(3),
   name: z.string().min(3),
 });
 
 export const CreateSignInSchema = z.object({
-  username: z.string().min(3).max(20),
+  email: z.string().email(),
   password: z.string().min(3),
-  name: z.string().min(3),
 });
 
 export const CreateRoomSchema = z.object({
