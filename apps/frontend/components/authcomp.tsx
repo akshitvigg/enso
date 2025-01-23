84fd1775071998dcd52c213deeec5c8ab9cd1563
Akshit Vig <akshitvig48@gmail.com>
2025-01-24 00:08:41 +0530

    merjafas

--- apps/frontend/components/authcomp.tsx
+++
@@ -1,9 +0,0 @@
-import { Input } from "@repo/ui/input";
-const AuthComp = ({ isSignin }: { isSignin: boolean }) => {
-  return (
-    <div className=" h-screen items-center justify-center flex">
-      <Input />
-    </div>
-  );
-};
-export default AuthComp;
