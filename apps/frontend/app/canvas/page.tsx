76a1d434fa55b1710709074340b94296e3fbadc7
Akshit Vig <akshitvig48@gmail.com>
2025-01-25 00:00:35 +0530

    canvas

---
+++ apps/frontend/app/canvas/page.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useEffect, useRef } from "react";
+
+export default function Canvas() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    if (canvasRef.current) {
+      const canvas = canvasRef.current;
+      const ctx = canvas.getContext("2d");
+      if (!ctx) {
+        return;
+      }
+
+      ctx.strokeRect(50, 50, 300, 300);
+    }
+  });
+
+  return (
+    <div className=" h-screen bg-fuchsia-300">
+      <canvas ref={canvasRef} width={1000} height={1000}></canvas>
+    </div>
+  );
+}
