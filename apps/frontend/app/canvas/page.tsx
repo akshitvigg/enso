f06580ab8d078a8ec4f3ddc920be922b80f88764
Akshit Vig <akshitvig48@gmail.com>
2025-01-26 02:12:57 +0530

    sete

--- apps/frontend/app/canvas/page.tsx
+++ apps/frontend/app/canvas/page.tsx
@@ -1,20 +1,11 @@
-"use client";
+import { SocketCanvas } from "@/components/SocketConnectCanvas";
 
-import { Draw } from "@/canvas/draw";
-import { useEffect, useRef } from "react";
-
-export default function Canvas() {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    if (canvasRef.current) {
-      Draw(canvasRef.current);
-    }
-  });
-
-  return (
-    <div className=" min-h-screen">
-      <canvas ref={canvasRef} width="2000" height={"2000"}></canvas>
-    </div>
-  );
+export default async function CanvasPage({
+  params,
+}: {
+  params: { roomId: string };
+}) {
+  const roomId = (await params).roomId;
+  console.log(roomId);
+  return <SocketCanvas roomId={roomId} />;
 }
