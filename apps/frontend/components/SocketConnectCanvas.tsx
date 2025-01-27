1bb97e0da231275d50c690739aac8dde3f625aea
Akshit Vig <akshitvig48@gmail.com>
2025-01-27 22:35:05 +0530

    jfksajf

--- apps/frontend/components/SocketConnectCanvas.tsx
+++ apps/frontend/components/SocketConnectCanvas.tsx
@@ -3,13 +3,9 @@
 import { useEffect, useState } from "react";
 import { Canvas } from "./realcanvas";
 import { WS_URL } from "@/config";
-import { Circle, Pencil, RectangleHorizontalIcon } from "lucide-react";
-
-type shapeType = "r" | "c" | "l";
 
 export function SocketCanvas({ roomId }: { roomId: string }) {
   const [socket, setSocket] = useState<WebSocket | null>(null);
-  const [currBtn, setcurrBtn] = useState<shapeType>();
 
   useEffect(() => {
     const ws = new WebSocket(
@@ -32,31 +28,6 @@ export function SocketCanvas({ roomId }: { roomId: string }) {
 
   return (
     <div className=" flex justify-center">
-      <div className=" bg-gray-900 p-1 rounded-lg fixed w-72  top-6">
-        <div className="gap-8  flex justify-center">
-          <div
-            onClick={() => setcurrBtn("r")}
-            className={` p-2 hover:bg-gray-600`}
-          >
-            <RectangleHorizontalIcon
-              color={`${currBtn === "r" ? "red" : "white"}`}
-            />
-          </div>
-          <div
-            onClick={() => setcurrBtn("c")}
-            className={`p-2 hover:bg-gray-600`}
-          >
-            <Circle color={`${currBtn === "c" ? "red" : "white"}`} />
-          </div>
-          <div
-            className={`p-2 hover:bg-gray-600`}
-            color={`${currBtn === "l" ? "red" : "white"}`}
-            onClick={() => setcurrBtn("l")}
-          >
-            <Pencil color={`${currBtn === "l" ? "red" : "white"}`} />
-          </div>
-        </div>
-      </div>
       <Canvas roomId={roomId} socket={socket} />
     </div>
   );
--- apps/frontend/components/realcanvas.tsx
+++ apps/frontend/components/realcanvas.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import { Draw } from "@/canvasLogic/draw";
-import { useEffect, useRef } from "react";
+import { Circle, Pencil, RectangleHorizontalIcon } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+
+type shapeType = "r" | "c" | "l";
 
 export function Canvas({
   roomId,
@@ -11,6 +14,7 @@ export function Canvas({
   socket: WebSocket;
 }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [currBtn, setcurrBtn] = useState<shapeType>();
 
   useEffect(() => {
     if (canvasRef.current) {
@@ -24,6 +28,38 @@ export function Canvas({
   return (
     <div className=" overflow-hidden">
       <canvas ref={canvasRef} width={width} height={height}></canvas>
+    </div>
+  );
+}
+
+function ToolBar() {
+  return (
+    <div>
+      <div className=" bg-gray-900 p-1 rounded-lg fixed w-72  top-6">
+        <div className="gap-8  flex justify-center">
+          <div
+            onClick={() => setcurrBtn("r")}
+            className={` p-2 hover:bg-gray-600`}
+          >
+            <RectangleHorizontalIcon
+              color={`${currBtn === "r" ? "red" : "white"}`}
+            />
+          </div>
+          <div
+            onClick={() => setcurrBtn("c")}
+            className={`p-2 hover:bg-gray-600`}
+          >
+            <Circle color={`${currBtn === "c" ? "red" : "white"}`} />
+          </div>
+          <div
+            className={`p-2 hover:bg-gray-600`}
+            color={`${currBtn === "l" ? "red" : "white"}`}
+            onClick={() => setcurrBtn("l")}
+          >
+            <Pencil color={`${currBtn === "l" ? "red" : "white"}`} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
