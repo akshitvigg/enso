30ba78078fcc6a39ab11d1a99762290bee4fe409
Akshit Vig <akshitvig48@gmail.com>
2025-01-31 00:55:56 +0530

    checkpoint

--- apps/frontend/components/realcanvas.tsx
+++ apps/frontend/components/realcanvas.tsx
@@ -1,10 +1,11 @@
 "use client";
 
 import { Draw } from "@/canvasLogic/draw";
+import { Game } from "@/canvasLogic/games";
 import { Circle, Pencil, RectangleHorizontalIcon } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 
-type ToolsType = "r" | "c" | "l";
+export type ToolsType = "rect" | "circle" | "pencil";
 
 export function Canvas({
   roomId,
@@ -14,11 +15,21 @@ export function Canvas({
   socket: WebSocket;
 }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
-  const [selectedTool, setselectedTool] = useState<ToolsType>("r");
+  const [game, setGame] = useState<Game>();
+  const [selectedTool, setselectedTool] = useState<ToolsType>("rect");
+
+  useEffect(() => {
+    game?.setTool(selectedTool);
+  }, [selectedTool, game]);
 
   useEffect(() => {
     if (canvasRef.current) {
-      Draw(canvasRef.current, roomId, socket);
+      const g = new Game(canvasRef.current, roomId, socket);
+      // Draw(canvasRef.current, roomId, socket);
+
+      return () => {
+        g.destroy();
+      };
     }
   }, [canvasRef]);
 
@@ -45,25 +56,25 @@ function ToolBar({
       <div className=" bg-gray-900 p-1 rounded-lg fixed w-72  top-6">
         <div className="gap-8  flex justify-center">
           <div
-            onClick={() => setselectedTool("r")}
+            onClick={() => setselectedTool("rect")}
             className={` p-2 hover:bg-gray-600`}
           >
             <RectangleHorizontalIcon
-              color={`${selectedTool === "r" ? "red" : "white"}`}
+              color={`${selectedTool === "rect" ? "red" : "white"}`}
             />
           </div>
           <div
-            onClick={() => setselectedTool("c")}
+            onClick={() => setselectedTool("circle")}
             className={`p-2 hover:bg-gray-600`}
           >
-            <Circle color={`${selectedTool === "c" ? "red" : "white"}`} />
+            <Circle color={`${selectedTool === "circle" ? "red" : "white"}`} />
           </div>
           <div
             className={`p-2 hover:bg-gray-600`}
-            color={`${selectedTool === "l" ? "red" : "white"}`}
-            onClick={() => setselectedTool("l")}
+            color={`${selectedTool === "pencil" ? "red" : "white"}`}
+            onClick={() => setselectedTool("pencil")}
           >
-            <Pencil color={`${selectedTool === "l" ? "red" : "white"}`} />
+            <Pencil color={`${selectedTool === "pencil" ? "red" : "white"}`} />
           </div>
         </div>
       </div>
