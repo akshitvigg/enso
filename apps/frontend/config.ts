1804ef98300ce09b30173f43f918a2346dd87b80
Akshit Vig <akshitvig48@gmail.com>
2025-04-04 20:45:39 +0530

    added deployed urls

--- apps/frontend/config.ts
+++ apps/frontend/config.ts
@@ -1,2 +1,2 @@
-export const WEB_URL = "http://localhost:3002";
-export const WS_URL = "ws://localhost:8080";
+export const WEB_URL = "https://enso-http-be.onrender.com";
+export const WS_URL = "wss://enso-sockets.onrender.com";
